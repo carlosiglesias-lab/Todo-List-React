e717b27a83754fdd204131d6657c317f2112837e
carlosiglesias-lab <carlos.igles@gmail.com>
2022-12-15 14:54:00 +0000

    ok

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,33 +1,56 @@
-import React, { useState } from "react";
-import Card from "./card";
+import React, { useEffect, useState } from "react";
+
 const Home = () => {
-	const [todos, setTodos] = useState(["Eat", "Drink", "Sleep"]);
+	const [todos, setTodos] = useState([{ "label": "Loading Tasks", "done": false }]);
+
+	useEffect(() => {
+		getTodos();
+
+	}, []);
+
+	const getTodos = async () => {
+		const response = await fetch("https://assets.breatheco.de/apis/fake/todos/user/carlos", { method: 'GET' });
+		const data = await response.json();
+		setTodos(data);
+		console.log(data);
+	}
+	useEffect(() => {
+		updateTodos();
+	}, [todos]);
+	
+	const updateTodos = async () => {
+		await fetch("https://assets.breatheco.de/apis/fake/todos/user/carlos",
+			{
+				method: 'PUT',
+				body: JSON.stringify(todos),
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			});
+	}
 
 	return (
 		<div id="container">
 			<h1 className="todo-header">To do List</h1>
 			<input id="addToDo" type="text" placeholder="Add to do here" onKeyUp={(e) => {
-				if (e.key == "Enter" && e.target.value.trim() != "" && todos.filter((item,i) => item == e.target.value)== ""){
-					setTodos([...todos, e.target.value]);
-					console.log(todos.filter(item => item == e.target.value));		
-					//todos.filter(item => item == e.target.value)== "" ? console.log("vacio") : console.log("lleno");
+				if (e.key == "Enter" && e.target.value.trim() != "" && todos.filter((item, i) => item.label == e.target.value) == "") {
+					setTodos([...todos, { "label": e.target.value, "done": false }]);
 					e.target.value = "";
 				}
-				
 			}
 			} />
 			<ul>
 				{todos.map((todo, index) => {
 					return (
 						<li key={index}>
-							<span><i className="fa fa-trash" onClick={(e)=>{
-								setTodos(todos.filter((e,i) => i !== index));								
-							}}></i></span>{todo}
+							<span><i className="fa fa-trash" onClick={(e) => {
+								setTodos(todos.filter((e, i) => i !== index));														
+							}}></i></span>{todo.label}
 						</li>
 					)
 				})}
 			</ul>
-			<p>{todos.length > 0 ? todos.length+" tasks left" : "No tasks, add a task"} </p>
+			<p>{todos.length > 0 ? todos.length + " tasks left" : "No tasks, add a task"} </p>
 		</div>
 	);
 };
